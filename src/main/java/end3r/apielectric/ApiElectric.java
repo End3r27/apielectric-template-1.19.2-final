8310d6962f520b69cd8a6fc2199e64944a4c2a40
End3r <76972137+End3r27@users.noreply.github.com>
2025-04-09 21:55:08 +0200

    0.0.0.1

--- src/main/java/end3r/apielectric/ApiElectric.java
+++ src/main/java/end3r/apielectric/ApiElectric.java
@@ -1,9 +1,5 @@
 package end3r.apielectric;
 
-import end3r.apielectric.registry.ModBlockEntities;
-import end3r.apielectric.registry.ModBlocks;
-import end3r.apielectric.registry.ModItems;
-import end3r.apielectric.screen.ModScreens;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +10,7 @@ public class ApiElectric implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// Register blocks, items, block entities, and screens
-		ModBlocks.registerBlocks();
-		ModItems.registerItems();  // Register items before item group
-		ModBlockEntities.registerBlockEntities();
-		ModScreens.registerAll();
+
 
 
 		LOGGER.info("Hello Fabric world!");
