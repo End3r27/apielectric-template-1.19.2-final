c01698aef6a89ad397ed9960f3248840971898f1
end3r_27 <valerio.virzi1@gmail.com>
2025-04-12 19:45:24 +0200

    Enhanced Comb Capacitor with energy transfer system and visual feedback

--- src/main/java/end3r/apielectric/block/CombCapacitorBlock.java
+++ src/main/java/end3r/apielectric/block/CombCapacitorBlock.java
@@ -6,14 +6,18 @@ import end3r.apielectric.registry.ModBlockEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class CombCapacitorBlock extends Block implements BlockEntityProvider {
+public class CombCapacitorBlock extends BlockWithEntity implements BlockEntityProvider {
 
     public CombCapacitorBlock(Settings settings) {
         super(settings);
@@ -25,11 +29,19 @@ public class CombCapacitorBlock extends Block implements BlockEntityProvider {
         return new CombCapacitorBlockEntity(pos, state);
     }
 
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        // This now works because checkType is inherited from BlockWithEntity
+        return checkType(type, ModBlockEntities.COMB_CAPACITOR_ENTITY,
+                (tickWorld, pos, tickState, blockEntity) -> CombCapacitorBlockEntity.tick(tickWorld, pos, tickState, blockEntity));
+    }
+
     @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         if (state.getBlock() != newState.getBlock()) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof EnergyApiaryBlockEntity apiary) {
+            if (blockEntity instanceof CombCapacitorBlockEntity apiary) {
                 ItemStack stack = new ItemStack(this);
                 NbtCompound nbt = new NbtCompound();
                 nbt.putInt("HoneyCharge", apiary.getStoredHoneyCharge());
@@ -48,7 +60,7 @@ public class CombCapacitorBlock extends Block implements BlockEntityProvider {
 
             // Get the block entity and set the charge
             BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof EnergyApiaryBlockEntity apiaryEntity) {
+            if (blockEntity instanceof CombCapacitorBlockEntity apiaryEntity) {
                 apiaryEntity.setStoredHoneyCharge(storedCharge);
             }
         }
--- src/main/java/end3r/apielectric/block/entity/CombCapacitorBlockEntity.java
+++ src/main/java/end3r/apielectric/block/entity/CombCapacitorBlockEntity.java
@@ -3,13 +3,137 @@ package end3r.apielectric.block.entity;
 
 import end3r.apielectric.registry.ModBlockEntities;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 
 public class CombCapacitorBlockEntity extends BaseHoneyChargeBlockEntity {
 
-    public static final int MAX_HONEYCHARGE = 8000;
+    public static final int MAX_HONEYCHARGE = 80000;
+    private static int transferCooldown = 0;
+    private static final int TRANSFER_COOLDOWN_MAX = 20; // Transfer every 20 ticks (1 second)
+
 
     public CombCapacitorBlockEntity(net.minecraft.util.math.BlockPos pos, BlockState state) {
         super(ModBlockEntities.COMB_CAPACITOR_ENTITY, pos, state, MAX_HONEYCHARGE);
     }
+
+    @Override
+    public void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        nbt.putInt("HoneyCharge", this.getStoredHoneyCharge());
+        nbt.putInt("TransferCooldown", this.transferCooldown);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        this.setStoredHoneyCharge(nbt.getInt("HoneyCharge"));
+        this.transferCooldown = nbt.getInt("TransferCooldown");
+    }
+
+    public void receiveEnergy(int amount) {
+        this.addHoneyCharge(amount);
+        markDirty();
+
+        if (world != null && !world.isClient) {
+            // Spawn particles
+            ((ServerWorld) world).spawnParticles(
+                    ParticleTypes.HAPPY_VILLAGER, // or CUSTOM one!
+                    pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5,
+                    5,  // count
+                    0.2, 0.3, 0.2, // offset
+                    0.01  // speed
+            );
+
+            // Play a sound
+            world.playSound(
+                    null, // player (null = everyone nearby hears it)
+                    pos,
+                    SoundEvents.ENTITY_BEE_POLLINATE,
+                    SoundCategory.BLOCKS,
+                    0.7f, // volume
+                    1.2f  // pitch
+            );
+        }
+    }
+
+    /**
+     * Transfers honey charge to adjacent blocks that can receive it
+     */
+    public void transferHoneyChargeToAdjacentBlocks() {
+        if (world == null || world.isClient || getStoredHoneyCharge() <= 0) {
+            return; // Don't run on client side or if we have no charge
+        }
+
+        // The amount of energy to transfer per block
+        int transferAmount = 50;
+
+        // Check all adjacent blocks
+        BlockPos[] adjacentPositions = new BlockPos[] {
+                pos.north(), pos.south(), pos.east(), pos.west(), pos.up(), pos.down()
+        };
+
+        for (BlockPos adjacentPos : adjacentPositions) {
+            BlockEntity adjacentEntity = world.getBlockEntity(adjacentPos);
+
+            // Check if the adjacent block can receive honey charge
+            if (adjacentEntity instanceof HoneyChargeFurnaceBlockEntity furnace) {
+                // Calculate how much we can actually transfer
+                int chargeToTransfer = Math.min(transferAmount, getStoredHoneyCharge());
+                if (chargeToTransfer > 0) {
+                    // Transfer the charge
+                    furnace.receiveHoneyCharge(chargeToTransfer);
+                    // Reduce our stored charge
+                    consumeHoneyCharge(chargeToTransfer);
+
+                    // Visual and sound effects
+                    ((ServerWorld) world).spawnParticles(
+                            ParticleTypes.FALLING_HONEY,
+                            pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+                            5, // count
+                            0.5, 0.5, 0.5, // offset
+                            0.01 // speed
+                    );
+
+                    world.playSound(
+                            null, // No specific player
+                            pos,
+                            SoundEvents.BLOCK_HONEY_BLOCK_SLIDE,
+                            SoundCategory.BLOCKS,
+                            0.5f, // volume
+                            1.0f + world.getRandom().nextFloat() * 0.2f // pitch with variation
+                    );
+
+                    markDirty();
+                }
+            }
+        }
+    }
+
+    /**
+     * Static tick method called by the BlockEntityTicker
+     */
+    public static void tick(World world, BlockPos pos, BlockState state, CombCapacitorBlockEntity entity) {
+        if (world.isClient()) return;
+
+        // Decrement cooldown if active
+        if (transferCooldown > 0) {
+            transferCooldown--;
+        }
+
+        // When cooldown reaches zero, try to transfer energy
+        if (transferCooldown <= 0 && entity.getStoredHoneyCharge() > 0) {
+            entity.transferHoneyChargeToAdjacentBlocks();
+            // Reset cooldown
+            transferCooldown = TRANSFER_COOLDOWN_MAX;
+        }
+    }
 }
+
